2db3fd880b508f5e12161b0fd64507d48cbf8cae
Сайтама <skyrim2012@gmail.com>
2021-06-12 08:53:12 +0300

    update MosGorsudService

--- bot.ts
+++ bot.ts
@@ -3,7 +3,7 @@ import { Context } from 'telegraf';
 import { config } from './shared/config';
 import { TelegramBot } from './shared/core/bot/TelegramBot';
 import { MainController, ReportController } from './controllers';
-import { MosGorsudService } from './shared/services/MosGorsudService';
+import { CourtCase, MosGorsudService } from './shared/services/MosGorsudService';
 import { MessageBuilder } from './shared/core/classes';
 
 export const BOT = new TelegramBot(config.bot.token);
@@ -16,14 +16,20 @@ BOT.on('message', async (ctx: Context) => {
 
 	const data = await MosGorsudService.search(text);
 
-	const message = data.message[0];
+	const reportMessage = data.slice(0, 5).map((courtCase: CourtCase) => {
+		const report = `<b>Запрос: ${courtCase.person} - ${courtCase.court}</b>
+		<b>Категория дела: ${courtCase.type}</b>
+		<b>Статус: ${courtCase.status}</b>
+		Номер дела: <a href="${courtCase.url}">${courtCase.caseNumber}</a>
+		Текущее состояние: ${courtCase.state}
+		Судья: ${courtCase.judge || '---'}
+		Статья: ${courtCase.codexArticle || '---'}
+		Категория дела: ${courtCase.category || '---'}`;
 
-	const report = `${message.category.snippetText}\n
-	${message.participantsName.snippetText}\n
-	${message.publishingStateDescription.snippetText}\n
-	${message.firstCourt.snippetText}`;
+		return report;
+	}).join('\n\n');
 
-	await ctx.reply(MessageBuilder.compile(report));
+	await ctx.replyWithHTML(MessageBuilder.compile(reportMessage));
 });
 
 BOT.catch(console.error);
--- shared/services/MosGorsudService.ts
+++ shared/services/MosGorsudService.ts
@@ -1,19 +1,100 @@
 import axios from 'axios';
 
+// -------------------------------------
+
+export interface CourtCase {
+	status: string;
+	person: string;
+	type: string;
+	category: string;
+	caseNumber: string;
+	state: string;
+	url: string;
+	court: string;
+	judge?: string;
+	codexArticle?: string;
+}
+
+// -------------------------------------
 export class MosGorsudService {
 	static readonly API_URL: string = 'https://mos-gorsud.ru';
 
-	static async search(text: string) {
+	static async search(searchText: string) {
 		const url = `${this.API_URL}/search.php`;
 
 		const response = await axios.get(url, {
 			params: {
-				q: text,
+				q: searchText,
 				offset: 0,
 				limit: 15,
 			},
 		});
 
-		return response.data;
+		return this.parseData(response.data.message, searchText);
 	}
+
+	private static parseData(data: Record<string, any>[], searchText: string) {
+		const result: CourtCase[] = [];
+
+		for (const rawItem of data) {
+			const { status, person } = this.getPersonAndStatus(rawItem.participantsWithCategories, searchText);
+
+			if (person.length === 0) {
+				continue;
+			}
+
+			const courtCase: CourtCase = {
+				status,
+				person,
+				type: this.getValue(rawItem.type.snippetText),
+				category: this.getValue(rawItem.category.snippetText),
+				caseNumber: this.getValue(rawItem.fullNumberSearch.snippetText),
+				url: `${this.API_URL}${rawItem.url}`,
+				state: this.getValue(rawItem.publishingStateDescription.snippetText),
+				court: rawItem.court.snippetText,
+				judge: rawItem.judge.snippetText ? this.getValue(rawItem.judge.snippetText) : undefined,
+				codexArticle: rawItem.codexArticle.snippetText ? this.getValue(rawItem.codexArticle.snippetText) : undefined,
+			};
+
+			result.push(courtCase);
+		}
+
+		return result;
+	}
+
+	private static getValue(message: string, separator = ':'): string {
+		const index = message.indexOf(separator);
+
+		if (index === -1) {
+			return message;
+		}
+
+		let value = message.slice(index + 1).trim();
+
+		value = value.replace('<span class="highlight">', '');
+		value = value.replace('</span>', '');
+
+		return value;
+	}
+
+	private static getPersonAndStatus(participants: Record<string, string>, searchText: string) {
+		let status: string = '', person: string = '';
+
+		for (const key of Object.keys(participants)) {
+			if (typeof participants[key] !== 'string') {
+				continue;
+			}
+
+			if (participants[key].toLowerCase().includes(searchText.toLowerCase()) === true) {
+				status = key;
+				person = participants[key];
+
+				break;
+			}
+		}
+
+		return { status, person };
+	}
+
+	private constructor() {}
 }
